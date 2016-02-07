13c61ebb22edb3a4927e0048ff26fa77f5680126
oliverfei <ofei006@uottawa.ca>
2016-02-06 22:23:34 -0500

    testing

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,9 +4,9 @@ function findSoulmates(){
     $.get({
         url: "https://docs.google.com/spreadsheets/d/14sOjj8_pFvMm_ekbR9a8P_gsjDx4ONaO5fYDB02_Asw/pubhtml?gid=1282985650&single=true",
         success: function(data){
-            var html = $.parseHTML(data);
-            var tableData = $(html).find("td");
-            Console.log(JSON.stringify(tableData));
+            //var html = $.parseHTML(data);
+            //var tableData = $(html).find("td");
+            Console.log(JSON.stringify(data));
         }
     });
 }
